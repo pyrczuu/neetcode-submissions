9bcbd5f9236eb8681bf4acb53d1f81bef7075c60
Damian Pyrcz <dydekpyrcz@gmail.com>
2026-04-03 14:52:08 +0200

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> Integer.compare(nums[b], nums[a]));
+        int[] res = new int[nums.length-k+1];
+        for (int i = 0; i < k; i++) {
+            queue.add(i);
+        }
+        res[0] = nums[queue.peek()];
+
+        for (int i = 1; i < nums.length - k + 1; i++) {
+            queue.add(i+k-1);
+            while (i > queue.peek()) {
+                queue.poll();
+            }
+            queue.remove(i-1);
+            res[i] = nums[queue.peek()];
+        }
+        return res;
+    }
+}
