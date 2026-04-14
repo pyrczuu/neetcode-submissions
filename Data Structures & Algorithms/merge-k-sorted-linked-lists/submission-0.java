b179d3ce556d3df913a151ee9d0412b1ff4e8efa
Damian Pyrcz <dydekpyrcz@gmail.com>
2026-04-14 11:03:39 +0200

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        while (lists.length > 1) {
+            ArrayList<ListNode> mergedLists = new ArrayList<>();
+
+            for (int i = 0; i < lists.length; i+=2) {
+                ListNode l1 = lists[i];
+                ListNode l2 = (i + 1) < lists.length ? lists[i+1] : null;
+                mergedLists.add(mergeTwoLists(l1,l2));
+            }
+            lists = mergedLists.toArray(new ListNode[0]);
+        }
+        return lists[0];
+    }
+    
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) { return list2;};
+        if (list2 == null) { return list1;};
+        ListNode head;
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+        ListNode cur = head;
+            
+        while (cur != null) {
+            if (list1 == null) {
+                cur.next = list2;
+                return head; 
+            }
+            if (list2 == null) {
+                cur.next = list1;
+                return head;
+            }
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
