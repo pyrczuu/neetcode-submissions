f07cf7e3f1a33fb80bae0729a85632998b23a7fa
Damian Pyrcz <dydekpyrcz@gmail.com>
2026-05-24 20:33:00 +0200

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        StringBuilder sb = new StringBuilder();
+        helper(sb, 0, 0, n);
+        return res; 
+    }
+    private void helper(StringBuilder sb, int open, int close, int n) {
+        if (sb.length() == 2 * n) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (open < n) {
+            sb.append('(');
+            helper(sb, open + 1, close, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        if (close < open) {
+            sb.append(')');
+            helper(sb, open, close + 1, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
