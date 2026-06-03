4c8963504b408ba2f94d612970cfb2162f4f481f
Damian Pyrcz <dydekpyrcz@gmail.com>
2026-06-03 18:23:40 +0200

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int time = 0;
+        int fresh = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                switch (grid[i][j]) {
+                    case 1:
+                    fresh++;
+                    break;
+                    case 2:
+                    q.add(new int[] {i, j});
+                    default: 
+                    break;
+                }
+            }
+        }
+
+        int[][] directions = {{1, 0}, {-1, 0},
+                            {0, 1}, {0, -1}};
+
+        while (fresh > 0 && !q.isEmpty()) {
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                for (int[] dir : directions) {
+                    int row = r + dir[0];
+                    int col = c + dir[1];
+                    if (row >= 0 && col >= 0 && row < grid.length &&
+                        col < grid[0].length && grid[row][col] == 1) {
+                        grid[row][col] = 2;
+                        q.add(new int[] {row, col});
+                        fresh--;
+                    }
+                }
+            }
+            time++;
+        }
+        return fresh == 0 ? time : -1;
+    }
+
+}
